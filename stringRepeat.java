d785598aefb47280b09ead9f32d625e7abdde11c
mohammadkhan <mohammadkhan@nypl.org>
2019-10-28 19:30:47 -0400

    repeat a given string

---
+++ stringRepeat.java
@@ -0,0 +1,8 @@
+    public static String repeatStr(final int repeat, final String string) {
+        String repeatWord = "";
+        for(int i=0; i<repeat; i++){
+          repeatWord+=string;
+        }
+        return repeatWord;
+        
+    }
