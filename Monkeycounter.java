3496e71a49b6580cba388c898d4f671becac5f0b
mohammadkhan <mohammadkhan@nypl.org>
2019-10-29 08:04:59 -0400

    Count the monkeys

---
+++ Monkeycounter.java
@@ -0,0 +1,10 @@
+public class MonkeyCounter
+{
+  public static int[] monkeyCount(final int n){
+      int arr[] = new int[n];
+      for(int i=1; i<=n; i++){
+        arr[i-1] += i;
+      }
+      return arr;
+  }
+}
