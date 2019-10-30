ee65579c659a1ce766c938d2846928a9a27ba226
mohammadkhan <mohammadkhan@nypl.org>
2019-10-30 07:45:15 -0400

    changes to Monkeycounter.java

--- Monkeycounter.java
+++ Monkeycounter.java
@@ -2,7 +2,7 @@ public class MonkeyCounter
 {
   public static int[] monkeyCount(final int n){
       int arr[] = new int[n];
-      for(int i=1; i<=n; i++){
+      for(int i=1;i<=n;i++){
         arr[i-1] += i;
       }
       return arr;
